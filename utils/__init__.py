004a4bd71010f32480e95f9ec8c089c7f3f8bec6
Danil Tumanov <dania.tumanov@gmail.com>
2023-11-17 19:27:14 +0200

    Add utils module

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .api_utils import get_hero_by_id
+from .file_utils import write_hero_info_to_csv, download_hero_img
+
---
+++ utils/file_utils.py
@@ -0,0 +1,53 @@
+from typing import Any, Optional
+import requests
+from config import ROOT
+import pandas as pd
+from pathlib import Path
+
+IMG_PATH = ROOT / 'img'
+CSV_PATH = ROOT / 'csv'
+
+
+def download_hero_img(hero_name: str, img_url: str) -> Optional[Path]:
+    """
+    Download an image from the given URL and save it with the specified name.
+
+    Parameters:
+        hero_name (str): The name to be used for saving the image.
+        img_url (str): The URL of the image to be downloaded.
+
+    Returns:
+        Optional[Path]: The path where the image is saved if successful, else None.
+    """
+    IMG_PATH.mkdir(exist_ok=True)
+    response = requests.get(img_url)
+    if response.status_code == 200:
+        with open(IMG_PATH / f'{hero_name}.png', 'wb') as file:
+            file.write(response.content)
+        print(f"Image downloaded and saved at {IMG_PATH}")
+        return IMG_PATH / f'{hero_name}.png'
+    else:
+        print(f"Failed to download image from {img_url}")
+        return None
+
+
+def write_hero_info_to_csv(hero_info: Any, csv_name: str) -> None:
+    """
+    Write hero information to a CSV file.
+
+    Parameters:
+        hero_info (Any): The hero information to be written to the CSV file.
+        csv_name (str): The name of the CSV file.
+
+    Returns:
+        None
+    """
+    CSV_PATH.mkdir(exist_ok=True)
+    df = pd.json_normalize(hero_info)
+    csv_file_path = CSV_PATH / f'{csv_name}.csv'
+    try:
+        existing_df = pd.read_csv(csv_file_path)
+        updated_df = pd.concat([existing_df, df], ignore_index=True)
+    except FileNotFoundError:
+        updated_df = df
+    updated_df.to_csv(csv_file_path, index=False)
