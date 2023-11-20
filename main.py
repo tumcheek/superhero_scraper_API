4dc08e273d6213c2bd919cfe7e611e702c7567e6
Danil Tumanov <dania.tumanov@gmail.com>
2023-11-20 09:41:55 +0200

    Delete outdate comments

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scraper import process_hero_response
 if __name__ == '__main__':
     hero_numbers = int(input('number of heroes='))
     start_from = int(input('start='))
-    csv_name = input('csv file name=')  # Fixed spelling error here
+    csv_name = input('csv file name=')
     for hero_number in range(start_from, hero_numbers + start_from):
         try:
             hero_info = process_hero_response(get_hero_by_id(hero_number))
